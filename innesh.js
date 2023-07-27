afc00b0f6084e85bc18bb02d560719f17a69f802
Glitch (valiant-lofty-drifter) <none>
2023-07-27 18:58:41 +0000

    🐂🚴 Checkpoint ./innesh.js:29446382/4

--- innesh.js
+++ innesh.js
@@ -90,7 +90,7 @@ var ownerid = "920006201184514078";
 
 setInterval(function(){if(db.get("huntbot") != "kapalı"){innesh.channels.cache.get(huntkanal).send("owo h");};},17500);
 setInterval(function(){if(db.get("huntbot") != "kapalı"){innesh.channels.cache.get(huntkanal).send("owo");};},11000);
-setInterval(function(){if(db.get("huntbot") != "kapalı"){innesh.channels.cache.get(huntkanal).send("owo battle");};},18400);
+setInterval(function(){if(db.get("huntbot") != "kapalı"){innesh.channels.cache.get(huntkanal).send("owo battle");};},18600);
 setInterval(function(){if(db.get("huntbot") != "kapalı"){innesh.channels.cache.get(huntkanal).send("owo pray");};},300000);
 
 
